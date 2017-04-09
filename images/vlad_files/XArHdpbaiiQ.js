97dcf735e242cfd450e0940bb53b0cb9efb0621b
Juan Sierra <jasierra.garcia@gmail.com>
2017-04-09 03:36:05 -0400

    Small changes

--- images/vlad_files/XArHdpbaiiQ.js
+++
@@ -1,9 +0,0 @@
-if (self.CavalryLogger) { CavalryLogger.start_js(["8oJIO"]); }
-
-/** js/bugs/bug.js */
-
-
-
-__d('legacy:bug-js',['BugBar'],(function a(b,c,d,e,f,g){
-
-b.BugBar=b.BugBar||c('BugBar');}),3);
--- images/vlad_files/iTOR1mbs2a8.js
+++
@@ -1,8 +0,0 @@
-if (self.CavalryLogger) { CavalryLogger.start_js(["8eJ2b"]); }
-
-/** js/photos/viewer/PhotoSnowliftAds.js */
-
-
-__d('legacy:PhotoSnowliftAds',['PhotoSnowliftAds'],(function a(b,c,d,e,f,g){
-
-b.PhotoSnowliftAds=c('PhotoSnowliftAds');}),3);
